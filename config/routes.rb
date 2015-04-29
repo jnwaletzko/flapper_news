e073603ba36b0ffc642e7842d3f333f1e727f140
jnwaletzko <jnwaletzko@gmail.com>
2015-04-29 16:55:47 -0500

    Create rails controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,16 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root to: 'application#angular'
+  
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:show, :create] do
+      member do
+        put '/upvote' => 'comments#upvote'
+      end
+    end
+    
+    member do 
+      put '/upvote' => 'posts#upvote'
+    end  
+  end
 end
